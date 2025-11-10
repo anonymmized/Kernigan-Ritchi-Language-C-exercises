330e4be3ad71e065a2897d91ce525b1b0c7c1bc5
anonymmized <lih0deev@inbox.ru>
2025-11-10 15:42:51 +0300

    add 3.5 ex

---
+++ part3_d/part3_5.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include <stdio.h>
+
+void reverse(char s[]) {
+    int i, j, c;
+    for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
+
+int main() {
+    char s[100] = "hello";
+    reverse(s);
+    printf("%s\n", s);
+    return 0;
+}
