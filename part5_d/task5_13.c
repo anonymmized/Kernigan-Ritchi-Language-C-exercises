d44c12e7271a812ac29bb5780d0cde6fd0b52e87
anonymmized <lih0deev@inbox.ru>
2025-12-01 21:44:41 +0300

    add 5.13 ex

--- part5_d/task5_13.c
+++ part5_d/task5_13.c
@@ -1,47 +1,53 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-#define MAXLINE 1000
 #define MAXLINES 100
+#define MAXLINE 1000
+#define LAST 10
 
-char lines[MAXLINES][MAXLINE];
-int liner = 0;
-
-void tail(int n) {
-    int start = liner > n ? liner - n : 0;
-    for (int i = start; i < liner; i++) {
-        printf("%s", lines[i]);   // \n уже есть в строке
-    }
-}
-
-int mgetline(char *line, int lim) {
+int mgetline(char *s, int lim) {
     int c, i = 0;
-    while (--lim > 0 && (c = getchar()) != EOF && c != '\n')
-        line[i++] = c;
-    if (c == '\n')
-        line[i++] = '\n';
-    line[i] = '\0';
+    while (--lim > 0 && (c = getchar()) != EOF && c != '\n') s[i++] = c;
+    if (c == '\n') {
+        s[i++] = '\n';
+    }
+    s[i] = '\0';
     return i;
 }
 
-int my_atoi(char *s) {
+int aatoi(char *s) {
     int n = 0;
-    for (int i = 0; s[i] >= '0' && s[i] <= '9'; i++)
-        n = 10 * n + (s[i] - '0');
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (s[i] >= '0' && s[i] <= '9') {
+            n = 10 * n + (s[i] - '0');
+        }
+    }
     return n;
 }
 
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("usage: %s n\n", argv[0]);
-        return 1;
+    int len;
+    char line[MAXLINE];
+    int last_strings = (argc < 2) ? LAST : aatoi(argv[1]);
+    if (last_strings <= 0) last_strings = LAST;
+    char *buffer[last_strings];
+    int i = 0;
+    while ((len = mgetline(line, MAXLINE)) > 0) {
+        char *p = malloc(strlen(line) + 1);
+        strcpy(p, line);
+        buffer[i % last_strings] = p;
+        i++;
+    }
+    int start = (i >= last_strings) ? i % last_strings : 0;
+    int count = (i < last_strings) ? i : last_strings;
+    for (int j = 0; j < count; j++) {
+        int idx = (j + start) % last_strings;
+        printf("%s", buffer[idx]);
+    }
+    for (int j = 0; j < count; j++) {
+        int idx = (j + start) % last_strings;
+        free(buffer[idx]);
     }
 
-    int n = my_atoi(argv[1]);
-
-    while (liner < MAXLINES && mgetline(lines[liner], MAXLINE) > 0)
-        liner++;
-
-    tail(n);
-    return 0;
 }
-    
